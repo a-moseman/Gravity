c5b18ecb47e306a08bbfe4458607c36d2db0311e
a-moseman <drewmoseman@gmail.com>
2023-10-23 17:07:51 -0400

    Minor refactor of UpdateLoop.

--- src/gravity/core/UpdateLoop.java
+++ src/gravity/core/UpdateLoop.java
@@ -5,12 +5,14 @@ import javax.swing.*;
 public class UpdateLoop implements Runnable {
     private static final long NANOS_PER_SECOND = 1_000_000_000;
     private final double TARGET_TPS;
+    private final long TARGET_TICK_TIME;
     private final JFrame FRAME;
     private final Simulation SIMULATION;
     private boolean running;
 
     public UpdateLoop(double targetTPS, JFrame frame, Simulation simulation) {
         TARGET_TPS = targetTPS;
+        TARGET_TICK_TIME = (long) (NANOS_PER_SECOND / TARGET_TPS);
         FRAME = frame;
         SIMULATION = simulation;
     }
@@ -19,18 +21,21 @@ public class UpdateLoop implements Runnable {
     public void run() {
         running = true;
         long tickStartTime;
-        long tickTime;
+        long tickComputeTime;
         double deltaTime = 1.0 / TARGET_TPS;
         while (running) {
             tickStartTime = System.nanoTime();
             SIMULATION.update(deltaTime);
             FRAME.repaint();
-            while ((tickTime = System.nanoTime() - tickStartTime) < NANOS_PER_SECOND / TARGET_TPS) {}
-            deltaTime = (double) tickTime / NANOS_PER_SECOND;
+            tickComputeTime = System.nanoTime() - tickStartTime;
+            while ((System.nanoTime() - tickStartTime) < TARGET_TICK_TIME) {
+
+            }
+            deltaTime = (double) Math.max(tickComputeTime, TARGET_TICK_TIME) / NANOS_PER_SECOND;
         }
     }
 
-    private void stop() {
+    public void stop() {
         running = false;
     }
 }
